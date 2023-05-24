25fd0c9f3e120913e84406894baf5239483ca895
Iñaki García <igardiet@gmail.com>
2023-05-24 23:57:54 +0200

    FEAT: gifs context hook

---
+++ client/src/hooks/useGifsContext.js
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import { GifsContext } from '../context/GifContext';
+
+export const useGifsContext = () => {
+  const context = useContext(GifsContext);
+
+  if (!context) {
+    throw Error('Gif context must be used inside gifsContextProvider');
+  }
+  return context;
+};
