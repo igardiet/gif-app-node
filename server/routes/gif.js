4259e3270da8fff080b7c64087de72caae78ddd1
Iñaki García <igardiet@gmail.com>
2023-05-24 23:55:34 +0200

    FEAT: get, post, update and delete gif routes

---
+++ server/routes/gif.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { requireAuth } = require('../middleware/requireAuth');
+const {
+  postGif,
+  getGifs,
+  getGif,
+  updateGif,
+  deleteGif,
+} = require('../controllers/gifController');
+
+router.get('/', getGifs);
+router.get('/:id', getGif);
+router.post('/', requireAuth, postGif);
+router.patch('/:id', requireAuth, updateGif);
+router.delete('/:id', requireAuth, deleteGif);
+
+module.exports = router;
