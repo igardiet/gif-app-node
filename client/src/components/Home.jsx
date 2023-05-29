50e7000fae5a45584859c51d901f1fa4ef363798
Iñaki García <igardiet@gmail.com>
2023-05-29 22:25:53 +0200

    FEAT: category renamed to gifFile and gif

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
 import { useGifsContext } from '../hooks/useGifsContext';
-import { GifDetails } from '../components/GifDetails';
 
 export const Home = () => {
   const { gifs, dispatch } = useGifsContext();
-console.log(gifs);
+
   useEffect(() => {
     const fetchGifs = async () => {
       try {
@@ -19,31 +18,28 @@ console.log(gifs);
     fetchGifs();
   }, [dispatch]);
 
-  const gifsByCategory = {};
+  const gifFile = {};
   gifs.forEach((gif) => {
-    const { category, username } = gif;
-    if (!gifsByCategory[category]) {
-      gifsByCategory[category] = {};
+    const { username } = gif;
+    if (!gifFile[gif]) {
+      gifFile[gif] = {};
     }
-    if (!gifsByCategory[category][username]) {
-      gifsByCategory[category][username] = [];
+    if (!gifFile[gif][username]) {
+      gifFile[gif][username] = [];
     }
-    gifsByCategory[category][username].push(gif);
+    gifFile[gif][username].push(gif);
   });
 
   return (
     <>
-      {Object.entries(gifsByCategory).map(([category, categoryGifs], index) => (
-        <div key={category + index}>
-          <h2>{category}</h2>
+      {Object.entries(gifFile).map(([gif, categoryGifs], index) => (
+        <div key={gif + index}>
+          <h2>{gif}</h2>
           {Object.entries(categoryGifs).map(([username, userGifs], index) => (
             <div key={username + index}>
               <h3>Uploaded by: {username}</h3>
               {userGifs.map((gif) => (
-                console.log(gif)
-                // <div>
-                //   <GifDetails gif={gif} key={gif._id} />
-                // </div>
+                <GifDetails gif={gif} key={gif._id} />
               ))}
             </div>
           ))}
