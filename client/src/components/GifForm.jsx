4d5ca8906c3cf4f508ffedde4338a326b9757b67
Iñaki García <igardiet@gmail.com>
2023-05-29 22:24:44 +0200

    CHORE: category useState, category formData, and input category removed

--- client/src/components/GifForm.jsx
+++ client/src/components/GifForm.jsx
@@ -6,7 +6,6 @@ export const GifForm = () => {
   const { dispatch } = useGifsContext();
   const { user } = useAuthContext();
 
-  const [category, setCategory] = useState('');
   const [img, setImg] = useState(null);
   const [error, setError] = useState(null);
   const [voidInput, setVoidInput] = useState([]);
@@ -20,37 +19,23 @@ export const GifForm = () => {
     }
 
     const formData = new FormData();
-    formData.append('category', category);
     formData.append('img', img);
     formData.append('email', user.email);
 
-    // const response = await fetch('http://localhost:3000/api/gifs', {
-    //   method: 'POST',
-    //   body: formData,
-    //   headers: {
-    //     Authorization: `Bearer ${user.token}`,
-    //   },
-    // });
-
-    // const gif = {category, img };
     const response = await fetch('http://localhost:3000/api/gifs', {
       method: 'POST',
       headers: {
-        // 'Content-Type': 'application/json',
-        'Authorization': `Bearer ${user.token}`,
+        Authorization: `Bearer ${user.token}`,
       },
       body: formData,
     });
-    
-    const json = await response.json();
-    console.log(json);
 
+    const json = await response.json();
     if (!json.success) {
       setError(json.error);
       setVoidInput(json.voidInput);
     }
     if (json.success) {
-      setCategory('');
       setImg(null);
       setError(null);
       setVoidInput([]);
@@ -58,26 +43,37 @@ export const GifForm = () => {
     }
   };
 
+  const handleFileChange = (e) => {
+    setImg(e.target.files[0]);
+  };
+
   return (
-    <form className='p-6' onSubmit={handleSubmit}>
-      <h3 className='text-black'>Upload a new gif!</h3>
+    <form
+      className='gif-form mx-auto my-8 p-4 relative rounded-xl'
+      onSubmit={handleSubmit}
+    >
+      <h2 className='text-2xl text-black mb-4'>Add a GIF</h2>
       <input
-        placeholder='Category'
-        type='text'
-        onChange={(e) => setCategory(e.target.value)}
-        value={category}
-        className={voidInput.includes('category') ? 'error' : ''}
-      />
-      <input
-        placeholder='Image'
-        type='file'
-        onChange={(e) => setImg(e.target.files[0])}
         className={voidInput.includes('img') ? 'error' : ''}
+        type='file'
+        name='img'
+        id='img'
+        accept='.gif'
+        onChange={handleFileChange}
       />
-      <button className='bg-black text-white cursor-pointer border-none p-2.5 rounded'>
-        Add Gif!
+      <button
+        className='bg-black text-white cursor-pointer border-none p-2.5 rounded'
+        type='submit'
+        disabled={!img}
+      >
+        Upload
       </button>
-      {error && <div className='error'>{error}</div>}
+      {error && <p className='text-red'>{error}</p>}
+      {voidInput.map((_, index) => (
+        <div key={index} className='mb-2'>
+          <input type='text' value='' className='hidden' disabled />
+        </div>
+      ))}
     </form>
   );
 };
