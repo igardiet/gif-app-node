a050dcff3ac2e32ed043f9e94134f942edd1cb87
Iñaki García <igardiet@gmail.com>
2023-05-31 17:14:52 +0200

    FEAT: SearchBar component render

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
+import { SearchBar } from './SearchBar';
 
 export const Navbar = () => {
   const { logout } = useLogout();
@@ -13,24 +14,25 @@ export const Navbar = () => {
   return (
     <header className='bg-black'>
       <div className='container max-w-full m-auto px-8 py-4 flex items-center justify-between'>
-        <Link className='no-underline text-white' to='/'>
-          <h1>GifApp</h1>
+        <Link to='/'>
+          <h1 className='no-underline text-white'>GifApp</h1>
         </Link>
+        <div className='flex mr-22 items-center text-white'>
+          <Link className='mx-8' to='/akira'>
+            Akira
+          </Link>
+          <Link className='mx-8' to='/dragonball'>
+            Dragon Ball
+          </Link>
+          <Link className='mx-8' to='/naruto'>
+            Naruto
+          </Link>
+        </div>
+        <SearchBar />
         <nav className='flex items-center'>
           {user && (
             <>
-              <div className='mr-[250px]'>
-                <Link className='mx-[32px]' to='/akira'>
-                  Akira
-                </Link>
-                <Link className='mx-[32px]' to='/dragonball'>
-                  Dragon Ball
-                </Link>
-                <Link className='mx-[32px]' to='/naruto'>
-                  Naruto
-                </Link>
-              </div>
-              <div>
+              <div className='flex items-center ml-auto'>
                 <span className='text-white'>{user.email}</span>
                 <button className='bg-white' onClick={handleClick}>
                   <span className='text-black'>Log out</span>
@@ -39,10 +41,10 @@ export const Navbar = () => {
             </>
           )}
           {!user && (
-            <div>
+            <>
               <Link to='/login'>Login</Link>
               <Link to='/signup'>Signup</Link>
-            </div>
+            </>
           )}
         </nav>
       </div>
