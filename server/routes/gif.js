89827f85ae2c021518953a16189f1cd79cb2b0dc
Iñaki García <igardiet@gmail.com>
2023-06-01 15:12:56 +0200

    FEAT: getGifsByCategory and searchGifs routes

--- server/routes/gif.js
+++ server/routes/gif.js
@@ -9,12 +9,16 @@ const {
   getGif,
   updateGif,
   deleteGif,
+  getGifsByCategory,
+  searchGifs,
 } = require('../controllers/gifController');
 
 router.get('/', getGifs);
 router.get('/:id', getGif);
+router.get('/category/:category', getGifsByCategory);
+router.get('/search/:query', searchGifs);
 router.post('/', requireAuth, postGif);
-router.patch('/:id', requireAuth, updateGif);
+router.put('/edit', requireAuth, updateGif);
 router.delete('/:id', requireAuth, deleteGif);
 
 module.exports = router;
