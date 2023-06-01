f07a1e819b46bc569f761aa2d19385b0d887aab1
Iñaki García <igardiet@gmail.com>
2023-06-01 15:38:13 +0200

    FEAT: useGifsContext dispatch removed

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,14 +1,11 @@
 import { useAuthContext } from './useAuthContext';
-import { useGifsContext } from './useGifsContext';
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
-  const { dispatch: dispatchGifs } = useGifsContext();
 
   const logout = () => {
     localStorage.removeItem('user');
     dispatch({ type: 'LOGOUT' });
-    dispatchGifs({ type: 'SET_GIFS', payload: null });
   };
   return { logout };
 };
