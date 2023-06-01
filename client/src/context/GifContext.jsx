2ffe6dba8f7fe225804952f451919998941dfbd8
Iñaki García <igardiet@gmail.com>
2023-06-01 15:56:50 +0200

    FEAT: fetchGifs, fetchGifsByCategory, searchGifs, editGif, dispatch functions

--- client/src/context/GifContext.jsx
+++ client/src/context/GifContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useContext, useReducer } from 'react';
+import { AuthContext } from './AuthContext';
 
 export const GifsContext = createContext();
 export const gifsReducer = (state, action) => {
@@ -24,9 +25,78 @@ export const GifsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(gifsReducer, {
     gifs: null,
   });
+  const { user } = useContext(AuthContext);
+
+  const fetchGifs = async () => {
+    const response = await fetch('http://localhost:3000/api/gifs');
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'SET_GIFS', payload: json });
+    }
+  };
+
+  const fetchGifsByCategory = async (category) => {
+    const response = await fetch(
+      `http://localhost:3000/api/gifs/category/${category}`
+    );
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'SET_GIFS', payload: json });
+    }
+  };
+
+  const searchGifs = async (query) => {
+    const response = await fetch(
+      `http://localhost:3000/api/gifs/search/${query}`
+    );
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'SET_GIFS', payload: json });
+    }
+  };
+
+  const editGif = async (gifId, newTitle) => {
+    const gifData = { gifId, newTitle };
+    try {
+      const response = await fetch('http://localhost:3000/api/gifs/edit', {
+        method: 'PUT',
+        body: JSON.stringify(gifData),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const data = await response.json();
+
+      if (data?.ok) {
+        const updatedGifs = state.gifs.map((gif) => {
+          if (gif._id === gifId) {
+            return { ...gif, title: newTitle };
+          } else {
+            return gif;
+          }
+        });
+        dispatch({ type: 'SET_GIFS', payload: updatedGifs });
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
 
   return (
-    <GifsContext.Provider value={{ ...state, dispatch }}>
+    <GifsContext.Provider
+      value={{
+        ...state,
+        dispatch,
+        fetchGifs,
+        fetchGifsByCategory,
+        searchGifs,
+        editGif,
+      }}
+    >
       {children}
     </GifsContext.Provider>
   );
