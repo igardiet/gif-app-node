dfbce4af125b1f8ad4e9f9d035ec18e9933e6909
Iñaki García <igardiet@gmail.com>
2023-06-01 15:53:14 +0200

    FEAT: handleSearch by query

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,14 +1,31 @@
+import { useContext, useState } from 'react';
+import { GifsContext } from '../context/GifContext';
+
 export const SearchBar = () => {
+  const { searchGifs, fetchGifs } = useContext(GifsContext);
+  const [query, setQuery] = useState('');
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    const query = e.target.value;
+
+    if (query.length < 1) {
+      fetchGifs();
+      return;
+    }
+
+    searchGifs(e.target.value);
+  };
   return (
     <>
       <input
         className='w-[15%] h-6 mb-0'
         type='search'
         name='filter'
-        // value={query}
+        value={query}
         placeholder='Search'
         aria-label='Search'
-        // onChange={handleInput}
+        onChange={handleSearch}
       />
     </>
   );
