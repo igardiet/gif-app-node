473f1ffb05c904bad9ea250b881ae37a8c756c4a
Iñaki García <igardiet@gmail.com>
2023-06-02 14:05:24 +0200

    STYLE: span and button tailwind classes

--- client/src/components/AddGifModal.jsx
+++ client/src/components/AddGifModal.jsx
@@ -3,22 +3,18 @@ import { GifForm } from './GifForm';
 export const AddGifModal = ({ onClose }) => {
   return (
     <div className='fixed top-0 right-0 bottom-0 left-0 m-auto z-50 h-screen w-screen flex items-center justify-center backdrop-blur-sm'>
-      <div className='h-1/2 w-1/2 bg-white '>
-        <h5 className='modal-title'>Add Gif</h5>
+      <span className='h-1/2 w-1/2 bg-[#f1f2f6] rounded'>
         <button
           type='button'
-          className='close'
+          className='close pl-1'
           data-dismiss='modal'
           aria-label='Close'
           onClick={onClose}
         >
-          Close
+          X
         </button>
-
-        <div className='modal-body'>
-          <GifForm onClose={onClose} />
-        </div>
-      </div>
+        <GifForm onClose={onClose} />
+      </span>
     </div>
   );
 };
