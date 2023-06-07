964084583b3e7166dccdce4fedd0e65b10e2b593
Iñaki García <igardiet@gmail.com>
2023-06-07 12:37:53 +0200

    chore: add gif button and searchbar in div flex; isSearchBarVisible useLocation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useGifsContext, useAuthContext } from '../hooks';
 import { GifDetails, AddGifModal } from '../components';
+import { SearchBar } from '../components/SearchBar';
 
 export const Home = () => {
   const { gifs, fetchGifs } = useGifsContext();
   const { user } = useAuthContext();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const location = useLocation();
+
+  const isSearchBarVisible = location.pathname === '/';
 
   useEffect(() => {
     fetchGifs();
@@ -21,11 +26,14 @@ export const Home = () => {
 
   return (
     <>
-      {user && (
-        <button className='btn btn-danger' onClick={handleModalOpen}>
-          Add Gif
-        </button>
-      )}
+      <div className='flex'>
+        {user && (
+          <button className='btn btn-danger' onClick={handleModalOpen}>
+            Add Gif
+          </button>
+        )}
+        {isSearchBarVisible && <SearchBar />}
+      </div>
       {isModalOpen && <AddGifModal onClose={handleModalClose} />}
       <div className='grid gap-10 grid-cols-4'>
         {gifs && gifs.map((gif) => <GifDetails key={gif._id} gif={gif} />)}
