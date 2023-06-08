a8004a93ab3eb5eab1e22c63b2b3c9891cb0c0ae
Iñaki García <igardiet@gmail.com>
2023-06-08 13:12:42 +0200

    styles: backdrop-blur added to div class

--- src/components/AddGifModal.jsx
+++ src/components/AddGifModal.jsx
@@ -4,8 +4,7 @@ import { MdCancel } from 'react-icons/md';
 export const AddGifModal = ({ onClose }) => {
   return (
     <div
-      className='fixed top-0 right-0 bottom-0 left-0 m-auto z-50 h-screen w-screen flex items-center justify-center'
-      // backdrop-blur-sm
+      className='fixed top-0 right-0 bottom-0 left-0 m-auto z-50 h-screen w-screen flex items-center justify-center backdrop-blur-sm'
     >
       <span className='h-1/2 w-1/2 bg-[#f1f2f6] rounded border-3 border-[#303952]'>
         <button
